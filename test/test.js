e22b2356feb271d6bf70e57a513504c026363acb
next0262 <w.j.kim@kaist.ac.kr>
2015-01-07 22:31:11 +0900

    v1.0
    init

---
+++ test/test.js
@@ -0,0 +1,109 @@
+/**
+ * Created by wjkim on 1/6/15.
+ */
+
+Connection = require('./../xsclient')
+
+var XenServerClient = new Connection('143.248.152.82',49202);
+
+/*
+XenServerClient.loginWithPassword('root','ancl@2014', function(error, sessionRef){
+    if(error) {
+        console.log(error);
+    } else {
+        console.log(sessionRef);
+        XenServerClient.getALLRecords(sessionRef,function(error,recList){
+            if(error){
+                console.log(error);
+            } else{
+                var record;
+                for(key in recList){
+                    if(recList[key].domid > 0)
+                        record = recList[key];
+                }
+                console.log(record);
+
+                var cons;
+                conss = record.consoles;
+                for(key in conss){
+                    XenServerClient.getProtocol(sessionRef, conss[key], function(error, protocol){
+                        console.log(protocol);
+                        if(protocol =='rfb'){
+                            XenServerClient.getLocation(sessionRef, conss[key],function(error, location){
+                                console.log(location);
+                            });
+                        }
+
+                    });
+
+                }
+            }
+        });
+    }
+});
+*/
+/*
+XenServerClient.logout('OpaqueRef:fba734d0-ad0f-8eec-f5ac-4caf5708fb22', function(error, message){
+    if(error) {
+        console.log(error);
+    } else {
+        console.log(message);
+    }
+});
+*/
+
+/*
+XenServerClient.loginWithPassword('root', 'ancl@2014', function(error, sessionRef){
+    if(error){
+        console.log(error);
+    } else {
+        XenServerClient.getRecord(sessionRef,'OpaqueRef:a4f64f29-fa77-d347-f3c7-960b4a475e84', function(error, record){
+            if(error){
+                console.log(error);
+            } else {
+                console.log(record);
+            }
+        });
+    }
+});
+*/
+
+/*
+XenServerClient.loginWithPassword('root', 'ancl@2014', function(error, sessionRef){
+    if(error){
+        console.log(error);
+    } else {
+        console.log(sessionRef);
+        XenServerClient.getByUuid(sessionRef,'a9d498fc-dd66-b2a4-ab2e-829c3ffa9924', function(error, vm){
+            if(error){
+                console.log(error);
+            } else {
+                console.log(vm);
+            }
+        });
+    }
+});
+ */
+
+
+XenServerClient.loginWithPassword('root', 'ANCL@2014', function(error, sessionRef){
+    if(error){
+        console.log(error);
+    } else {
+        console.log(sessionRef);
+        XenServerClient.getALLRecords(sessionRef, function(error, recList){
+            if(error){
+                console.log(error);
+            } else{
+                for(key in recList){
+                    if(recList[key].domid > 0) {
+                        var record = recList[key];
+                        console.log(key);
+                        console.log(record)
+                    }
+                }
+            }
+
+        });
+    }
+});
---
+++ xsclient.js
@@ -0,0 +1,111 @@
+/**
+ * Created by wjkim on 1/6/15.
+ */
+xmlrpc = require('xmlrpc');
+
+function XenServerClient(host, port){
+
+    this.client = xmlrpc.createClient({ host: host, port: port, path: '/'});
+
+}
+//multi session available
+XenServerClient.prototype.loginWithPassword = function (username, password, callback){
+    this.client.methodCall('session.login_with_password', [username, password], function (error, response) {
+        if(error){
+         callback(new Error('BadServerResponse'));
+        }
+        if (response.Status == "Success")
+        {
+            callback(null,response.Value);
+
+        } else{
+            callback(new Error(response.ErrorDescription.toString()));
+        }
+    });
+};
+//empty value
+XenServerClient.prototype.logout = function (sessionRef, callback){
+   this.client.methodCall('session.logout', [sessionRef], function(error, response){
+       if(error){
+           callback(new Error('BadServerResponse'));
+       }
+       if (response.Status == "Success")
+       {
+           callback(null,"Logout Success");
+       } else{
+           callback(new Error(response.ErrorDescription.toString()));
+       }
+   });
+};
+
+XenServerClient.prototype.getByUuid= function (sessionRef, uuid, callback){
+    this.client.methodCall('VM.get_by_uuid', [sessionRef, uuid], function(error, response){
+        if(error){
+            callback(new Error('BadServerResponse'));
+        }
+        if (response.Status == "Success")
+        {
+            callback(null, response.Value);
+        } else{
+            callback(new Error(response.ErrorDescription.toString()));
+        }
+    });
+};
+
+XenServerClient.prototype.getALLRecords = function (sessionRef, callback){
+    this.client.methodCall('VM.get_all_records', [sessionRef], function(error, response){
+        if(error){
+            callback(new Error('BadServerResponse'));
+        }
+        if (response.Status == "Success")
+        {
+            callback(null, response.Value);
+        } else{
+            callback(new Error(response.ErrorDescription.toString()));
+        }
+    });
+};
+
+XenServerClient.prototype.getRecord = function (sessionRef, vmRef, callback){
+    this.client.methodCall('VM.get_record', [sessionRef, vmRef], function(error, response){
+        if(error){
+            callback(new Error('BadServerResponse'));
+        }
+        if (response.Status == "Success")
+        {
+            callback(null, response.Value);
+        } else{
+            callback(new Error(response.ErrorDescription.toString()));
+        }
+    });
+};
+
+XenServerClient.prototype.getProtocol = function (sessionRef, consoleRef, callback){
+    this.client.methodCall('console.get_protocol', [sessionRef, consoleRef], function(error, response){
+        if(error){
+            callback(new Error('BadServerResponse'));
+        }
+        if (response.Status == "Success")
+        {
+            callback(null, response.Value);
+        } else{
+            callback(new Error(response.ErrorDescription.toString()));
+        }
+    });
+};
+
+XenServerClient.prototype.getLocation= function (sessionRef, consoleRef, callback){
+    this.client.methodCall('console.get_location', [sessionRef, consoleRef], function(error, response){
+        if(error){
+            callback(new Error('BadServerResponse'));
+        }
+        if (response.Status == "Success")
+        {
+            callback(null, response.Value);
+        } else{
+            callback(new Error(response.ErrorDescription.toString()));
+        }
+    });
+};
+
+module.exports = XenServerClient
